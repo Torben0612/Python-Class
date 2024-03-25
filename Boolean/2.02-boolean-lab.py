e9ea4789e3e88a02b7ef0f5c64371e29c884481a
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-03-25 09:01:06 -0400

    update

--- Boolean/2.02-boolean-lab.py
+++ Boolean/2.02-boolean-lab.py
@@ -6,16 +6,23 @@ yearslived = int(input("How many years have you lived in us?"))
 yearslivedok = yearslived >= 14
 print("You have been a resident for at least 14 years:", yearslivedok)
 
-natborn = 
-
 
 #Part III
 print("Give me your height by giving total feed and then on the next prompt the remaining inches")
 heightFeet = int(input("How many feet tall are you? "))
 additonalInch = int(input("How many additional inches"))
 
-heightInch = heightfeet * 12 + additonalInch
+heightInch = heightFeet * 12 + additonalInch
 print("You are tall enough to ride the rolar coaster:", heightInch >= 50)
 
-#age = int(input("How old are you? "))
+age = int(input("How old are you? "))
+print("You are old enough to ride regarless of your height", age >= 18)
 
+money = int(input("How many quarters do you have"))
+print("You have enough money to ride without a pass:", money >= 4)
+
+ridePass = input("Do you have a frequent rider pass")
+
+print("You only need to pay two quarters to ride:", ridePass == "Yes" or "yes")
+
+print("You can ride the coaster:", (heightInch>=50 or age >= 18) and (money>=4 or (ridePass == "Yes" or "yes" and money >= 2)))
---
+++ While_Loops/format.py
@@ -0,0 +1,3 @@
+distances = [4, 3.5, 6]
+progress = .60
+print("over the last 3 days, I ran the following distances:", distances, "I am {:.0%} towards my weekly goal.".format(progress))
