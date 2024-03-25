eb22cc00f7d264db7f759e723bba9e2449002d84
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-03-25 09:12:20 -0400

    v

---
+++ While_Loops/2.06While-Loop-Lab.py
@@ -0,0 +1,7 @@
+coins = 22
+print("Take turns removing 1, 2, or 3 coins. You win if you take the last coin.")
+
+player1Name = input("Player 1 whats your name? ")
+player2Name = input("Player 2 whats your name? ")
+
+while 
