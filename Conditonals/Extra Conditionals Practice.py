0f9e73e4d3b4148774d33a428a99d833a4c7ed6a
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-03-27 11:08:53 -0400

    gwg

--- Conditonals/Extra Conditionals Practice.py
+++ Conditonals/Extra Conditionals Practice.py
@@ -1,12 +1,27 @@
-# #Change Temperature Units
+#Super Nice
+# number = int(input("Give me a Number: "))
 
-units = input("Would you like to enter a temperature in Fahrenheit or in Celsius (type F or C)?")
-if units == "F":
-    temp = float(input("Enter a Fahrenheit temperature:"))
-    print("The tempature is", (temp-32) * 5/9, "degrees Celsius.")
-elif units == "C":
-    temp = float(input("Enter a Celsius temperature:"))
-    print("The tempature is", (0/5 * temp) + 32, "degrees Fahrenheit.")
+# multiple3 = number % 3 == 0
+# multiple5 = number % 5 == 01
+# if multiple3 == True and multiple5 == True:
+#     print("Super Nice")
+# elif multiple3 == True:
+#     print("Super")
+# elif multiple5 == True:
+#     print("Nice")
+# else:
+#     print("Not Nice Number")
+
+
+# Change Temperature Units
+
+# units = input("Would you like to enter a temperature in Fahrenheit or in Celsius (type F or C)?")
+# if units == "F":
+#     temp = float(input("Enter a Fahrenheit temperature:"))
+#     print("The tempature is", (temp-32) * 5/9, "degrees Celsius.")
+# elif units == "C":
+#     temp = float(input("Enter a Celsius temperature:"))
+#     print("The tempature is", (0/5 * temp) + 32, "degrees Fahrenheit.")
 
 #Sorting
 # x = int(input("Enter a number: "))
@@ -34,7 +49,18 @@ elif units == "C":
 #     else:
 #         print(x,y,z)
 #         print(z,y,x)
-    
+
+#Leap Year
+year = int(input("Enter a Year: "))
+leapYear = year % 4
+noLeapYear = year % 100
+leapCentury = year % 400
+
+if leapYear == True and noLeapYear == True and leapCentury == True:
+    print(year, "is a leap year")
+elif leapYear == True and noLeapYear == True and leapCentury == False:
+    print(year, "is not a leap year")
+elif leapYear == False:
 #Grade Level
 # grade = int(input("What grade are you in: "))
 # if grade == 9:
