8cb0702028a1348919b055561ff7cea9344acc24
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-03-28 09:04:09 -0400

    ewfw2

---
+++ Lists/Lists-lab-q.py
@@ -0,0 +1,7 @@
+number_list = [1,2,3,4,5,6]
+print(number_list.index(2))
+print(number_list[2])
+number_list.pop()
+print(number_list)
+print(3 in [1,2,3,4])
+
--- Lists/list-part-2-lab.py
+++ Lists/list-part-2-lab.py
@@ -1,3 +1,9 @@
 numbersList = [[1, 3, 6], [6, 8, 3], 8]
 
 favNum = int(input("What is your fav number"))
+maxNum = numbersList[0][0]
+if maxNum < numbersList[0][1]:
+    maxNum = numbersList[0][1]
+elif maxNum < numbersList[0][2]:
+    maxNum = numbersList[0][2]
+
