f62fb0aa83cbe1f7c62e642890ccbe1d493db070
Torben0612 <torben.krah@gmail.com>
2024-03-31 12:04:59 -0400

    gh

--- Lists/list-part-2-lab.py
+++ Lists/list-part-2-lab.py
@@ -1,9 +1,20 @@
-numbersList = [[1, 3, 6], [6, 8, 3], 8]
+numbersList = [[1, 3, 6, 37], [6, 8, 9, 5], 2]
 
-favNum = int(input("What is your fav number"))
+favNum = int(input("What is your fav number? "))
+
 maxNum = numbersList[0][0]
 if maxNum < numbersList[0][1]:
     maxNum = numbersList[0][1]
-elif maxNum < numbersList[0][2]:
+if maxNum < numbersList[0][2]:
     maxNum = numbersList[0][2]
+if len(numbersList[0]) == 4 and maxNum < numbersList[0][3]:
+    maxNum = numbersList[0][3]
 
+prodNum = (numbersList[1][0] * numbersList[1][1] * numbersList[1][2])
+if len(numbersList[1]) == 4:
+    prodNum = prodNum * numbersList[1][3]
+
+if numbersList[2] % 2 == 0:
+    print("Your Luckey Number are", maxNum % favNum, "and", prodNum % favNum)
+else:
+    print("You are not luckey, have an unlucky day :)")
--- While_Loops/2.06While-Loop-Lab.py
+++ While_Loops/2.06While-Loop-Lab.py
@@ -4,4 +4,7 @@ print("Take turns removing 1, 2, or 3 coins. You win if you take the last coin."
 player1Name = input("Player 1 whats your name? ")
 player2Name = input("Player 2 whats your name? ")
 
-while 
+gameRunning = True
+
+while gameRunning:
+    
