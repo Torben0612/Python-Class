1088feb949c90dde35a1dd23c8c5ba470c14f804
Torben0612 <torben.krah@gmail.com>
2024-03-31 20:10:03 -0400

    g

--- While_Loops/2.06While-Loop-Lab.py
+++ While_Loops/2.06While-Loop-Lab.py
@@ -1,4 +1,6 @@
-coins = 22
+import random
+
+coins = random.randint(3,20)
 print("Take turns removing 1, 2, or 3 coins. You win if you take the last coin.")
 
 player1Name = input("Player 1 whats your name? ")
@@ -11,30 +13,47 @@ while gameRunning:
     player1Turn = True
     while player1Turn:
         print("There are", coins, "coins remaining.")
-        player1Coins = int(input(player1Name "How many coins do you take?"))
-        if player1Coins != 1 or 2 or 3 or player1Coins > coins:
+        player1Coins = int(input("{} How many coins do you take?".format(player1Name)))
+        if player1Coins > 3:
             print("That's not a legal move. Try again.")
-        coins = coins - player1Coins
-        if coins == 0:
-            winner = player1Name
-            gameRunning = False
-            player2Turn = False
+        elif player1Coins <= 0:
+            print("That's not a legal move. Try again.")
+        elif player1Coins > coins:
+            print("That's not a legal move. Try again.")
         else:
-            player2Turn = True
-            player1Turn = False
+            coins = coins - player1Coins
+            if coins == 0:
+                winner = player1Name
+                gameRunning = False
+                player2Turn = False
+                player1Turn = False
+                print("No more coins left")
+                print("{} wins".format(player1Name))
+                print("{} loses".format(player2Name))
+            else:
+                player2Turn = True
+                player1Turn = False
 
         
     while player2Turn:
         print("There are", coins, "coins remaining.")
-        player2Coins = input(int(player2Name, "How many coins do you take?"))
-        if player2Coins != 1 or 2 or 3 or player2Coins > coins:
+        player2Coins = int(input("{} How many coins do you take?".format(player2Name)))
+        if player2Coins > 3:
             print("That's not a legal move. Try again.")
-        coins = coins - player2Coins
-        if coins == 0:
-            winner = player2Name
-            gameRunning = False
-            player2Turn = False
-            player1Turn = False
+        elif player2Coins <= 0:
+            print("That's not a legal move. Try again.")
+        elif player2Coins > coins:
+            print("That's not a legal move. Try again.")
         else:
-            player2Turn = False
-            player1Turn = True
+            coins = coins - player2Coins
+            if coins == 0:
+                winner = player2Name
+                gameRunning = False
+                player2Turn = False
+                player1Turn = False
+                print("No more coins left")
+                print("{} wins".format(player2Name))
+                print("{} loses".format(player1Name))
+            else:
+                player2Turn = False
+                player1Turn = True
