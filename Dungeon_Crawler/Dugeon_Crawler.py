593c10cbb7ca5d34d08152987a04c358b7749d70
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-04-02 09:09:58 -0400

    r

--- Dungeon_Crawler/Dugeon_Crawler.py
+++ Dungeon_Crawler/Dugeon_Crawler.py
@@ -3,16 +3,13 @@ floor_1 = ['sword', 'stairs up', 'monster', 'stairs down', 'sword']
 floor_0 = ['nothing', 'sword', 'monster', 'stairs up', 'sword']
 dungeon = [floor_0, floor_1, floor_2]
 user_inventory = []
-
+choice = ""
+room = int
 print("""
-left, 
-right, 
-up, 
-down, 
-grab, 
-fight, 
-help 
-inventory
+left, right, up, down, 
+grab, fight, help, inventory
 """)
 game_running = True
+
 while game_running:
+    choice = input("You are You are in room {}".format())
