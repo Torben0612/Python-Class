7d13459c79db1bc37f8e2c87ae1bb7c45202f1ff
Torben0612 <torben.krah@gmail.com>
2024-04-09 18:54:00 -0400

    ffgg

---
+++ Binary_and_Hexadecimal/main.py
@@ -0,0 +1,15 @@
+floor_2 = ['magic_stones', 'stairs down', 'monster', 'boss_monster', 'prize']
+floor_1 = ['sword', 'stairs up', 'monster', 'stairs down', 'sword']
+floor_0 = ['nothing', 'sword', 'monster', 'stairs up', 'sword']
+dungeon = [floor_0, floor_1, floor_2]
+user_inventory = []
+choice = ""
+room = int
+print("""
+left, right, up, down, 
+grab, fight, help, inventory
+""")
+game_running = True
+
+while game_running:
+    choice = input("You are You are in room {}".format())
