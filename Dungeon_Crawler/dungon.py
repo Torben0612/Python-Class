dfa9eb73833e224a321761febebc97392aa0ed4b
Torben0612 <torben.krah@gmail.com>
2024-04-23 16:47:51 -0400

    werhe

--- Dungeon_Crawler/dungon.py
+++ Dungeon_Crawler/dungon.py
@@ -1,17 +1,29 @@
-floor_2 = ['a sword', 'stairs down', 'nothing', 'a monster boss', 'prize']
-floor_1 = ['magic stones', 'stairs up', 'a monster', 'stairs down', 'nothing']
-floor_0 = ['nothing', 'a sword', 'a monster', 'stairs up', 'a sword']
-dungeon = [floor_0, floor_1, floor_2]
+floor_3 = ['prize', 'monster boss', 'monster', 'sword', 'sword', 'stairs down']
+floor_2 = ['nothing', 'stairs down', 'sword', 'nothing', 'monster', 'stairs up']
+floor_1 = ['magic stones', 'stairs up', 'monster', 'stairs down', 'nothing', 'nothing']
+floor_0 = ['nothing', 'sword', 'monster', 'stairs up', 'sword', 'nothing']
+dungeon = [floor_0, floor_1, floor_2, floor_3]
 floor , room = 0,0
 inventory=[]
 lastMove="right"
-print("Welcome to Dungeon Crawler.\nYour command options are...\n\tleft\n\tright\n\tup\n\tdown\n\tgrab\n\tfight\n\thelp\n\tinventory")
+print("""Welcome to Dungeon Crawler. Your command options are...
+      left
+      right
+      up
+      down
+      grab
+      fight 
+      help
+      inventory
+
+      use help command to display more details of each command
+      """)
 
 
 while "prize" not in inventory:
   choice = input("\nYou are in a room with " + dungeon[floor][room] + ". What would you like to do?: ")
   if choice=="left":
-    if (dungeon[floor][room]=="a monster" or dungeon[floor][room]=="a monster boss") and lastMove=="left":
+    if (dungeon[floor][room]=="monster" or dungeon[floor][room]=="monster boss") and lastMove=="left":
       print("You cannot pass without defeating the " + dungeon[floor][room] + ".")      
     elif room!=0:
       room-=1
@@ -19,7 +31,7 @@ while "prize" not in inventory:
     else:
       print("There are no rooms to the left. Please make another choice.")
   elif choice=="right":
-    if (dungeon[floor][room]=="a monster" or dungeon[floor][room]=="a monster boss") and lastMove=="right":
+    if (dungeon[floor][room]=="monster" or dungeon[floor][room]=="monster boss") and lastMove=="right":
       print("You cannot pass without defeating the " + dungeon[floor][room] + ".")  
     elif room!=len(floor_0)-1:
       room+=1
@@ -37,25 +49,36 @@ while "prize" not in inventory:
     else:
       print("There are no stairs leading down in this room. Please make another choice")
   elif choice=="grab":
-    if dungeon[floor][room]=="magic stones" or dungeon[floor][room]=="a sword" or dungeon[floor][room]=="prize":
+    if dungeon[floor][room]=="magic stones" or dungeon[floor][room]=="sword" or dungeon[floor][room]=="prize":
       print("You grabbed " + dungeon[floor][room] + ".")
       inventory.append(dungeon[floor][room])
       dungeon[floor][room]="nothing"
     else:
       print("There is nothing to grab in this room. Please make another choice.")
   elif choice=="fight":
-    if dungeon[floor][room]=="a monster boss" and "a sword" in inventory and "magic stones" in inventory:
+    if dungeon[floor][room]=="monster boss" and "sword" in inventory and "magic stones" in inventory:
       dungeon[floor][room]="nothing"
-      inventory.remove("a sword")
+      inventory.remove("sword")
       print("You defeated the monster boss!")
-    elif dungeon[floor][room]=="a monster" and "a sword" in inventory:
+    elif dungeon[floor][room]=="monster" and "sword" in inventory:
       dungeon[floor][room]="nothing"
-      inventory.remove("a sword")
+      inventory.remove("sword")
       print("You defeated the monster!")
     else:
       print("You're missing the necessary items to fight. You must turn away.")
   elif choice=="help":
-    print("\nYour command options are...\n\tleft\n\tright\n\tup\n\tdown\n\tgrab\n\tfight\n\thelp\n\tinventory")
+    print("""
+      Welcome to Dungeon Crawler. help menu
+      Your command options are...
+      left - moves character left
+      right - moves character right
+      up - moves chacter up stairs if there
+      down -moves character down stairs if there
+      grab - grabs item in room if any
+      fight - fight monster (with sword) or monster boss (with sword and stones)
+      help - shows this help menu
+      inventory - displays current items in inventory
+      """)
   elif choice=="inventory":
     if len(inventory)==0:
       print("You are not carrying anything.")
