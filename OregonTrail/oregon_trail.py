a03d3b50ac660a3b62c833bd432df1cc7bce7c61
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-05-22 11:09:28 -0400

    f

--- OregonTrail/oregon_trail.py
+++ OregonTrail/oregon_trail.py
@@ -9,22 +9,30 @@ day = 0
 DAYS_IN_MONTH = [0,31,28,31,30,31,30,31,31,30,31,30,31] 
 
 def next_day():
-    food -= 1 
+    food -= 5
+    if day == loss_days[0] or day == loss_days[1]:
+        deduct_health()
 
-def advance_days():
-
+def advance_days(days):
+    while days != 0:
+        next_day()
+        days - 1
 
 def is_last_day():
-    
+    if day == DAYS_IN_MONTH[month]:
+        helth_loss_days()
+        month += 1
 
 def helth_loss_days():
     loss_days = [random.randint(0,DAYS_IN_MONTH[month]), random.randint(0,DAYS_IN_MONTH[month])]
     return loss_days
 
+def deduct_health():
+    health -= 1
 
 def status():
     print("""
-    Your status
+    Your status:
     health = {}
     food = {}
     distance remaining {}
@@ -33,10 +41,6 @@ def status():
 
 def help():
     print("""
-        The goal is to travel from St. Louis, Missouri to Oregon City Oregon (2000 miles) by Dec 31st. 
-        Each turn, the player is asked what action they choose. The player can type any of the following: travel, rest, hunt, status, help, or quit
-        The player's health randomly decreases 2 times during the month.
-        The player eats 5 lbs of food a day.
         travel: moves you randomly between 30-60 miles and takes 3-7 days (random).
         rest: increases health 1 level (up to 5 maximum) and takes 2-5 days (random).
         hunt: adds 100 lbs of food and takes 2-5 days (random).
@@ -47,11 +51,18 @@ def help():
 
 def hunt():
 
+
 def rest():
 
+
 def travel():
+    advance_days(random.randint(3,7))
+    distance_remain -= random.randint(30,60)
+
 
 def check_end():
+    if month == 12 and day == DAYS_IN_MONTH[12] and distance_remain == 0 and health >= 1:
+        print("You Win")
 
 #main program
 print("""The goal is to travel from St. Louis, Missouri to Oregon City Oregon
@@ -61,4 +72,4 @@ print("""The goal is to travel from St. Louis, Missouri to Oregon City Oregon
 player_name = input("What is your name")
 
 while True:
-    
+    status()
