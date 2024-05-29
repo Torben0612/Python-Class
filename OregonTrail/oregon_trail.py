3e7e6fd961f306034bd936bfe1aab967abed786e
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-05-29 09:14:34 -0400

    ff

--- OregonTrail/oregon_trail.py
+++ OregonTrail/oregon_trail.py
@@ -5,70 +5,91 @@ health = 5
 food = 500
 distance_remain = 2000
 month = 1
-day = 0
+day = 1
 DAYS_IN_MONTH = [0,31,28,31,30,31,30,31,31,30,31,30,31] 
-loss_days = []
+loss_days = [random.randint(1,DAYS_IN_MONTH[month]), random.randint(2,DAYS_IN_MONTH[month])]
 
 def next_day():
+    global day, loss_days, month
     deduct_food()
     if day == loss_days[0] or day == loss_days[1]:
         deduct_health()
     if is_last_day() == True:
         month += 1
-        day = 0
+        day = 1
         helth_loss_days()
+    day += 1
+    check_end()
     
 def advance_days(add_days):
-    while add_days != 0:
+    while add_days > 0:
         next_day()
-        add_days - 1
+        add_days -= 1
 
 #Name:
 #Purpose
 #Inputs
 #returns
 def is_last_day():
+    global day, month, DAYS_IN_MONTH
     if DAYS_IN_MONTH[month] == day:
         return True
     else:
         return False
 
 def helth_loss_days():
-    loss_days = [random.randint(0,DAYS_IN_MONTH[month]), random.randint(0,DAYS_IN_MONTH[month])]
+    global loss_days, month, DAYS_IN_MONTH
+    loss_days = [random.randint(1,DAYS_IN_MONTH[month]), random.randint(2,DAYS_IN_MONTH[month])]
 
 def deduct_health():
+    global health
     health -= 1
 
 def add_health():
+    global health
     health += 1
 
-
 def deduct_food():
+    global food
     food -= 5
 
 def status():
+    global health, food, distance_remain, month, day
     print("""
     Your status:
     health = {} | food = {} | distance remaining {} | date = {}/{}
     """.format(health, food, distance_remain, month, day))
 
 def hunt():
+    global food
     food += 100
     advance_days(random.randint(2,5))
 
 def rest():
-    advance_days(random.randint(2,5))
+    global health
     if health < 5:
         add_health()
-    else: "your already at max health of 5"
+    else:
+        print("your already at max health of 5")
+    advance_days(random.randint(2,5))
 
 def travel():
-    advance_days(random.randint(3,7))
+    global distance_remain
     distance_remain -= random.randint(30,60)
-
+    advance_days(random.randint(3,7))
 
 def check_end():
-    if month == 12 and day == DAYS_IN_MONTH[12] and distance_remain == 0 and health >= 1:
+    global month, day, DAYS_IN_MONTH, food, gameruning, health, distance_remain
+    if month == 12 and day == DAYS_IN_MONTH[12]:
+        print("You Lose you ran out of time")
+        gameruning = False
+    elif food <= 0:
+        print("You Lose, You Ran out of food")
+        gameruning = False
+    elif health == 0:
+        print("you dead.")
+        gameruning = False
+    elif distance_remain == 0 and health >= 1 and food >= 1:
         print("You Win")
 
 def help():
