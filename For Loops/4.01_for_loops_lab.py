13f7d27b132e25d9bd63e2e82d8cc8248e692843
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-06-03 08:32:23 -0400

    f

---
+++ For Loops/4.01_for_loops_lab.py
@@ -0,0 +1,13 @@
+list = ['a', 'b', 'c', 'd']
+
+# index = 0
+
+# while index < len(list):
+#     print(list[index])
+#     index = index + 1
+
+# for letter in list:
+#     print(letter)
+
+for letter in "python class":
+    print(letter)
