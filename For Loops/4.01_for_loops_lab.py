589cfe3aeb4686b224c262690149ebd050a2fcb1
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-06-05 11:16:40 -0400

    ff

--- For Loops/4.01_for_loops_lab.py
+++ For Loops/4.01_for_loops_lab.py
@@ -12,17 +12,53 @@
 # for letter in "python class":
 #     print("hi")
 
-names = []
+import random
+
 #FUNCTIONS
-
 #name:
-
 def get_numbers():
-    while runing == True:
-        num_list = []
+    num_list = []
+    while True:
         num = input("Please enter a integer or 'done' if finished: ")
-        num_list.append(num)
-        print(num_list)
+        if num == "done":
+            print(num_list)
+            return num_list
+        else:
+            num_list.append(int(num))
+    print("This the the list of numbers you inputed", num_list)
     return num_list
 
-get_numbers()
+def count_evens(num_list):
+    y = 0
+    for i in num_list:
+        if i % 2 == 0:
+            y += 1
+    print("the number of even numbers in the list is", y)
+    return y
+
+count_evens(get_numbers())
+
+#Part 2
+def name_list():
+    names = []
+    while True:
+        name = input("Please enter a name or 'done' if finished: ")
+        if name == "done":          
+            return names
+        else:
+            names.append(name)
+    return names
+
+def congrats(name_list):
+    events = ['on your first successful skydive', 'on getting your driver’s license', 'on getting straight A’s']
+    for i in name_list:
+        print("Congrats", i, events[random.randint(0,2)])
+
+congrats(name_list())
+
+
+#part 3
+
+#Name: count_letter
+#purpose: counts the number of letters
+#
